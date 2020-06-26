457f8339c2d1eb13988309c16a46ab50b5021cee
apurv910 <apurvsoni0910@gmail.com>
2020-06-26 23:56:28 +0530

    https://github.com/apurv910/protractorframework910.git

---
+++ conf/conf.js
@@ -0,0 +1,59 @@
+// An example configuration file.
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+  dest: 'target/screenshots',
+  filename: 'my-report.html'
+});
+
+
+exports.config = {
+  //seleniumAddress: 'http://localhost:4444/wd/hub',  
+  directConnect: true,
+  //specs: ['example_spec.js']
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome',
+    
+  },
+
+  // Framework to use. Jasmine is recommended.
+  //useAllAngular2AppRoots: true,
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['../tests/calculator.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  },
+
+  // Setup the report before any tests start
+  beforeLaunch: function() {
+    return new Promise(function(resolve){
+      reporter.beforeLaunch(resolve);
+    });
+  },
+
+  // Assign the test reporter to each running instance
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(reporter);
+  },
+
+  // Close the report after all tests finish
+  afterLaunch: function(exitCode) {
+    return new Promise(function(resolve){
+      reporter.afterLaunch(resolve.bind(this, exitCode));
+    });
+  },
+  onPrepare: function() {
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
+  }
+
+
+};
